c916f916cd97e188245acbb65b7c573e2dc743f3
mende2000 <dimitrimende@gmail.com>
2021-11-10 22:37:27 +0100

    reconnaissance afn

--- front_end/src/App.java
+++ front_end/src/App.java
@@ -1,58 +1,38 @@
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
-    @Override
-    public void start(final Stage primaryStage) {
+    public static void main(String args[]) throws IOException {
+        launch();
+        // Graph g = graph("test")
+        // .directed()
+        // .graphAttr()
+        // .with(Rank.dir(RankDir.LEFT_TO_RIGHT))
+        // .nodeAttr().with(name("arial"))
+        // .linkAttr().with("class", "class-link")
+        // .with(
+        // node("a").with(Color.RED).link(node("b")),
+        // node("b").link(node("c"))
+        // );
+        // Graphviz.fromGraph(g).height(100).render(Format.PNG).toFile(new
+        // File("./ex1.png"));
+        // //Graphviz.fromGraph(g).height(100).render(Format.PNG).toFile(new
+        // File("example/ex1.png"));
 
-        Button button = new Button();
-        button.setText("Open a New Window");
-
-        button.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-
-                Label secondLabel = new Label("I'm a Label on new Window");
-
-                StackPane secondaryLayout = new StackPane();
-                secondaryLayout.getChildren().add(secondLabel);
-
-                Scene secondScene = new Scene(secondaryLayout, 230, 100);
-
-                // New window (Stage)
-                Stage newWindow = new Stage();
-                newWindow.setTitle("Second Stage");
-                newWindow.setScene(secondScene);
-
-                // Set position of second window, related to primary window.
-                newWindow.setX(primaryStage.getX() + 200);
-                newWindow.setY(primaryStage.getY() + 100);
-
-                newWindow.show();
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(button);
-
-        Scene scene = new Scene(root, 450, 250);
-
-        primaryStage.setTitle("JavaFX Open a new Window (o7planning.org)");
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 
-    public static void main(String[] args) {
-        launch(args);
-        System.out.println("hello");
+    @Override
+    public void start(Stage stage) throws Exception {
+        AnchorPane root = FXMLLoader.load(getClass().getResource("/home/dimitri/automate_manip/front_end/src/ressource/window/mainView.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
--- front_end/src/com/automate/display/Test.java
+++ front_end/src/com/automate/display/Test.java
@@ -1,33 +1,6 @@
 package com.automate.display;
 
-import static guru.nidi.graphviz.model.Factory.graph;
-import static guru.nidi.graphviz.model.Factory.node;
-
-import java.io.File;
-import java.io.IOException;
-
-import guru.nidi.graphviz.attribute.Color;
-import guru.nidi.graphviz.attribute.Font;
-import guru.nidi.graphviz.attribute.Rank;
-import guru.nidi.graphviz.attribute.Rank.RankDir;
-import guru.nidi.graphviz.engine.Format;
-import guru.nidi.graphviz.engine.Graphviz;
-import guru.nidi.graphviz.model.Graph;
-
-public class Test {
-    public static void main(String args[]) throws IOException {
-       Graph g = graph("test")
-       .directed()
-       .graphAttr()
-       .with(Rank.dir(RankDir.LEFT_TO_RIGHT))
-       .nodeAttr().with(Font.name("arial"))
-       .linkAttr().with("class", "class-link")
-       .with(
-           node("a").with(Color.RED).link(node("b")),
-           node("b").link(node("c"))
-       );
-        Graphviz.fromGraph(g).height(100).render(Format.PNG).toFile(new File("./ex1.png"));
-       //Graphviz.fromGraph(g).height(100).render(Format.PNG).toFile(new File("example/ex1.png"));
-
-    }
+public class Test{
+   
+   
 }
